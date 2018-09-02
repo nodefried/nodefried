be7df8c60401f166cdf6e7d7ed20e1fe4498d3b7
nodefried <info@nodefried.com>
2018-09-02 06:21:54 -0500

    Adding screen startup for linux...

--- screen.sh
+++ screen.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 # Use script location as CWD
 cd "$(dirname "$0")"
-# Check if the screen is running
-if ! screen -list | grep -q "nodefried"; then
-  # Since it wasn't running, start it
-  screen -dmS nodefried /usr/bin/node init.js
+
+# Check for screen and Node
+if [ -x "$(command -v node)" ]; then
+  if [ -x "$(command -v screen)" ]; then
+    # Check if the screen is running
+    if ! screen -list | grep -q "nodefried"; then
+      # Since it wasn't running, start it
+      screen -dmS nodefried /usr/bin/node init.js
+    fi
+  fi
 fi
